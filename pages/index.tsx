ed527f3adf3866240561aa7addf1769efe684aee
Alan Astroga Hurtado <alanasto10@gmail.com>
2022-05-24 15:54:59 -0500

    empezando proyecto pokermo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,9 @@
 import type { NextPage } from "next";
-import Link from "next/link";
-import { MainLayout } from "../components/layouts/MainLayout";
 
 
 const Home: NextPage = () => {
   return (
-    <MainLayout>
-      
-        <h3 className={'title'}> Home Page </h3>
-        <h1 className={'title'}>
-          {/* Ir a <a href='/'> About</a> */}
-          Ir a<Link href="/about">About</Link>
-        </h1>
-      </MainLayout> 
+    <div><h1> Probando desde cero </h1></div>
   );
 };
 
